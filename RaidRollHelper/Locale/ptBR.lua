dbdf3181245aa8ac0aa340fc5284b64b8b6cd62c
Knoot <areuar@mail.ru>
2019-01-07 01:21:38 +0300

    locale

--- RaidRollHelper/Locale/ptBR.lua
+++ RaidRollHelper/Locale/ptBR.lua
@@ -1,24 +1,8 @@
 if not(GetLocale() == "ptBR") then
   return
 end
---[[
-ITEM_CLASS_NEED["Tecido"]=ITEM_CLASS_NEED["Тканевые"]
-ITEM_CLASS_NEED["Couro"]=ITEM_CLASS_NEED["Кожаные"]
-ITEM_CLASS_NEED["Malha"]=ITEM_CLASS_NEED["Кольчужные"]
-ITEM_CLASS_NEED["Placas"]=ITEM_CLASS_NEED["Латные"]
-ITEM_CLASS_NEED['Diversos']=ITEM_CLASS_NEED['Разное']
-]]
+
 local L = RAID_ROLL_HELPER.L
---SYSTEM
-L["Самоцвет"]="Gema"
-L["Реликвия артефакта"]="Relíquia de Artefato"
-L["Доспехи"]="Armadura"
-L["Тканевые"]="Tecido"
-L["Кожаные"]="Couro"
-L["Кольчужные"]="Malha"
-L["Латные"]="Placas"
-L["Разное"]="Diversos"
-L["Оружие"]="Armas"
 L["спеки"] = {
 	WARRIOR = {"Arms", "Fury", "Protection"},
 	DRUID = {"Balance", "Feral", "Guardian", "Restoration"},
@@ -34,50 +18,71 @@ L["спеки"] = {
 	SHAMAN = {"Elemental", "Enhancement", "Restoration"},
 }
 
--- error
-L["Предмет не найден в сумках"] = "Item not found in bags"
-L["Предмет не является частью экипировки"] = "The item is not an armor or a relic of an artifact"
+--SYSTEM
+L["игрока"] = "player"
+L["игроков"] = "players"
+L["Самоцвет"]="Gema"
+L["Реликвия артефакта"]="Relíquia de Artefato"
+L["Доспехи"]="Armadura"
+L["Тканевые"]="Tecido"
+L["Кожаные"]="Couro"
+L["Кольчужные"]="Malha"
+L["Латные"]="Placas"
+L["Разное"]="Diversos"
+L["Оружие"]="Armas"
 
--- help
+-- error
+L["Некорректная команда"] = "Invalid command"
+L["Предмет не найден в сумках"] = "Item not found in bags"
+L["Предмет не является частью экипировки"] = "The item is not an armor"
+
+-- FAQ
 L["itemLink help"] = "To get the |cFFFF99FFitemLink|r, click on the item with the |cFFFF99FFShift|r key pressed."
 L["вернуть окно ролла в середину экрана"] = "Return the roll window to the middle of the screen"
+L["команда предназначена для ознакомления с аддоном"] = "the command is intended for familiarization with addon"
+L["открыть окно настроек аддона"] = "open addon settings window"
 L["отправить предмет на ролл"] = "send item to the roll"
-L["подсчет количества игроков в рейде с аддоном Raid Roll Helper"] = "Counting the number of players in the raid with the addon"
-L["команда предназначена для ознакомления с аддоном"] = "команда предназначена для ознакомления с аддоном"
-L["открыть окно настроек аддона"] = "открыть окно настроек аддона"
+L["подсчет количества игроков в рейде с аддоном Raid Roll Helper"] = "Counting the number of players in the raid with the addon Raid Roll Helper"
 
 -- interface
-L["Закрыть"] = "Close"
-L["Нужно"] = "Need"
-L["Трансмог"] = "Transmog"
-L["Офф спек"] = "Off spec"
-L["Распылить"] = "Disenchant"
-L["Осталось: %u сек"] = "Remaining: %u sec "
-L["Отказаться"] = "Refuse"
-L["На трансмог"] = "On transmog"
-L["Разрешить роллить на трансмог"] = "Allow roll on transmog"
-L["На офф спек"] = "On off spec"
-L["Разрешить роллить на офф спек"] = "Allow roll on off spec"
-L["Отправить"] = "Send"
-L["Не отправлять"] = "Don't send"
 L["Заберите у %s"] = "Take away from %s"
+L["Закрыть"] = "Close"
+L["На офф спек"] = "On off spec"
+L["На трансмог"] = "On transmog"
+L["Не отправлять"] = "Don't send"
+L["Нужно"] = "Need"
 L["Оповещение о победе"] = "Win Alert"
+L["Осталось: %u сек"] = "Remaining: %u sec"
+L["Отказаться"] = "Refuse"
+L["Отправить"] = "Send"
+L["Офф спек"] = "Off spec"
 L["Показывать окно с оповещением о победе"] = "Show Win Alert Window"
 L["При получении добычи показывать окно с предложением выставить предмет на ролл"] = "When receiving loot, show a window with a proposal to put the item on the roll"
+L["Разрешить роллить на офф спек"] = "Allow roll on off spec"
+L["Разрешить роллить на трансмог"] = "Allow roll on transmog"
+L["Распылить"] = "Disenchant"
+L["Трансмог"] = "Transmog"
 
 -- print
-L["(На трансмог)"] = "(На трансмог)"
-L["(На офф спек)"] = "(На офф спек)"
-L["Некорректная команда"] = "Некорректная команда"
-L[" никому не нужно"] = " никому не нужно"
 L[" выбрасывает "] = " roll "
+L[" никому не нужно"] = " nobody needs"
 L[" победитель "] = " winner "
+L["(На офф спек)"] = "(On off spec)"
+L["(На трансмог)"] = "(On transmog)"
 L["Аддон Raid Roll Helper найден у %s."] = "Addon |cFFFF99FFRaid Roll Helper|r found in %s."
+L["Аддон установлен у:"] = "People with addon:"
 L["Ваша версия RaidRollHelper устарела."] = "|cFFFF99FFYou version of the RaidRollHelper out of date.|r"
-L["игрока"] = "player"
 L["Игроки без аддона %u: %s"] = "Players without addon %u: %s"
-L["игроков"] = "players"
+L["Новый ролл был отправлен. %s"] = "The new roll was sent. %s"
 L["Позиция по умолчанию"] = "Default window position"
 L["Ролл был успешно отправлен. %s"] = "The roll was successfully sent. %s"
-L["Новый ролл был отправлен. %s"] = "The new roll was sent. %s"
-L["Аддон установлен у:"] = "People with addon:"
+
+-- DEBUG
+L["Найдена новая версия"] = "New version found"
+L["неподходящая реликвия артефакта"] = "unsuitable artifact relic"
+L["неподходящий сет"] = "unsuitable set"
+L["неподходящий тип доспехов"] = "unsuitable type of armor"
+L["отправитель=игрок"] = "sender=player"
+L["пользователь уже отправлял этот предмет"] = "user already sent this item"
+L["Ролл не добавлен"] = "Roll was not added"
+L["Ролл спрятан"] = "Roll hidden"
